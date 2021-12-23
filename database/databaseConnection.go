f8fac6ff87d0532a3d4fa477d5e9492e206a8983
Bishal Adhikari <beeshal55@gmail.com>
2021-12-23 21:47:04 +0545

    update env_sample and remove redundant code from databaseConnection

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -6,16 +6,10 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func CreateDatabseConnection() *sql.DB {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
